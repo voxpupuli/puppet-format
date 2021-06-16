49a23a397cd8d7ea33f03ecae2472a6df34f9626
Tim Meusel <tim@bastelfreak.de>
2021-06-16 11:23:04 +0200

    fix tests for terminal-table 3

--- spec/functions/table_spec.rb
+++ spec/functions/table_spec.rb
@@ -13,7 +13,7 @@ describe 'format::table' do
     it { is_expected.to run.with_params(rows).and_return("+-----+---+\n| One | 1 |\n| Two | 2 |\n+-----+---+") }
     it {
       is_expected.to run.with_params('title' => 'title', 'rows' => rows).
-        and_return("+-----+---+\n|  title  |\n+-----+---+\n| One | 1 |\n| Two | 2 |\n+-----+---+")
+        and_return("+---------+\n|  title  |\n+-----+---+\n| One | 1 |\n| Two | 2 |\n+-----+---+")
     }
   end
 end
